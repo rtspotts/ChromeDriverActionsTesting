ede07e18ef5ad2754dd0cbbf502079b4b1773185
rtspotts <rtspotts@gmail.com>
2021-08-04 12:04:17 -0400

    Update google.steps.js

--- features/step_definitions/google.steps.js
+++ features/step_definitions/google.steps.js
@@ -13,8 +13,8 @@ options.addArguments("--no-sandbox");
 options.addArguments("--remote-debugging-port=9222");
 options.addArguments("--disable-dev-shm-usage");
 
-capabilities.set('chromeOptions', options);
-const driver = new Builder().withCapabilities(capabilities).forBrowser('chrome').build();
+//capabilities.set('chromeOptions', options);
+const driver = new Builder().setChromeOptions(options).forBrowser('chrome').build();
 
 Given('I am on the Google search page', {timeout: 60 * 1000}, async function () {
     await driver.get('http://www.google.com');
